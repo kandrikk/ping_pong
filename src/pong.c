49f43171a14d0707d71c59606bb713dfbac7a34f
Migdalia Fungus <migdalif@vo-k4.msk.21-school.ru>
2025-07-27 19:59:31 +0300

    +ncurses , управление и поле на нкерс

--- src/pong.c
+++ src/pong.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
+#include <ncurses.h>
 
 #define WIDTH 80
 #define HEIGHT 25
 #define MIDDLE 25 
 
-#define W_DIV 40
-#define H_DIV 25/2
+int x_ball = WIDTH/2;
+int y_ball = HEIGHT/2;
+int r_racket = HEIGHT/2;
+int l_racket = HEIGHT/2;
 
-int x_ball = W_DIV;
-int y_ball = H_DIV;
-int r_racket = H_DIV;
-int l_racket = H_DIV;
 int dx = 1;
 int dy = -1;
 
@@ -20,12 +19,14 @@ int r_res = 0;
 void draw_pong();
 void series();
 void pong();
-void clear();
-void move();
+void _move();
 void gameover();
 void p_win();
 
 int main() {
+    initscr();
+    noecho();
+    cbreak();
     pong();
     return 0;
 }
@@ -36,48 +37,41 @@ void pong() {
 }
 
 void draw_pong() {
+    clear();
+
     for (int y = 0; y < HEIGHT; ++y) {
         for (int x = 0; x < WIDTH; ++x) {
             if (y == 0 || y == HEIGHT - 1) {
-                printf("*");
+                mvaddch(y, x, '*');
             } else if (x == 0 || x == WIDTH - 1) {
-                printf("#");
+                mvaddch(y, x, '#');
             } else if (x == 3 && (y == l_racket || y == l_racket - 1 || y == l_racket + 1)) {
-                printf("\x1b[32m]\x1b[0m");
+                mvaddch(y, x, ']');
             } else if (x == WIDTH - 4 && (y == r_racket || y == r_racket - 1 || y == r_racket + 1)) {
-                printf("\x1b[32m[\x1b[0m"); 
+                mvaddch(y, x, '[');
             } else if (x == x_ball && y == y_ball) {
-                printf("\x1b[32m@\x1b[0m");
-            } else if (x == W_DIV) {
-                printf(".");
-            } else {
-                printf(" ");
+                mvaddch(y, x, '@');
+            } else if (x == WIDTH/2) {
+                mvaddch(y, x, '.');
             }
         }
-        printf("\n");
     }
-    printf("--------------------------------------------------------------------------------\n");
-    printf("Left player : %d\nRight players : %d\n", l_res, r_res);
-    printf("--------------------------------------------------------------------------------\n");
 }  
 
 void series() {
-    while(!(l_res >= 2 || r_res >= 2)) {
-        clear();
+    while(!(l_res >= 5 || r_res >= 5)) {
         draw_pong();
-        char c;
-        if (scanf("%c", &c) == 1) {
-            if (c == 'A' || c == 'a') --l_racket;
-            if (c == 'Z' || c == 'z') ++l_racket;
-            if (c == 'K' || c == 'k') --r_racket;
-            if (c == 'M' || c == 'm') ++r_racket;
-            if (c == 'Q' || c == 'q') break;
-        }
-        move();
+        char c = getch();
+        if (c == 'A' || c == 'a') --l_racket;
+        if (c == 'Z' || c == 'z') ++l_racket;
+        if (c == 'K' || c == 'k') --r_racket;
+        if (c == 'M' || c == 'm') ++r_racket;
+        if (c == 'Q' || c == 'q') break;
+        _move();
     }
 }
 
-void move() {
+void _move() {
     x_ball += dx;
     y_ball += dy;
 
@@ -97,33 +91,27 @@ void move() {
     }
 
     if (x_ball <= 1) {
-        x_ball = W_DIV;
-        y_ball = H_DIV;
+        x_ball = WIDTH/2;
+        y_ball = HEIGHT/2;
         r_res += 1;
     }
     
     if (x_ball >= WIDTH -2) {
-        x_ball = W_DIV;
-        y_ball = H_DIV;
+        x_ball = WIDTH/2;
+        y_ball = HEIGHT/2;
         l_res += 1;
     }
 
 }
 
 void gameover() {
-    printf("\x1b[31m");
-    printf("------------------------------------GAMEOVER------------------------------------\n\n");
     p_win(l_res, r_res);
 }
 
 void p_win(int a , int b) {
     if (a > b) {
-        printf("\x1b[32m------------------------------LEFT PLAYERS WIN!!!!------------------------------\x1b[0m\n\n");
+        printf("\x1b[32m------------------------LEFT PLAYERS WIN!!!!-------------------------\x1b[0m\n\n");
     } else {
-        printf("\x1b[32m------------------------------RIGHT PLAYERS WIN!!!!------------------------------\x1b[0m\n\n");
+        printf("\x1b[32m------------------------RIGHT PLAYERS WIN!!!!-------------------------\x1b[0m\n\n");
     }
-}
-
-void clear() {
-    for (int i = 0; i < 5; ++i) printf("\n");
 }

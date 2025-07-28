49761700c1438903cadf96c62d8e251b8a6876ca
Migdalia Fungus <migdalif@vo-l1.msk.21-school.ru>
2025-07-28 12:55:07 +0300

    Update Счета

--- src/pong.c
+++ src/pong.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
 #include <ncurses.h>
+#include <stdio.h>
 
 #define WIDTH 80
 #define HEIGHT 25
 
-int x_ball = WIDTH/2;
-int y_ball = HEIGHT/2;
+int x_ball = WIDTH / 2;
+int y_ball = HEIGHT / 2;
 int r_racket = 11;
 int l_racket = 11;
 
@@ -30,22 +30,21 @@ int main() {
 void pong() {
     initscr();
     start_color();
-    init_pair(1, COLOR_YELLOW, COLOR_YELLOW); // поле верх низ право лево
-    init_pair(2, COLOR_RED, COLOR_RED);  // ракетки left
-    init_pair(22, COLOR_BLUE, COLOR_BLUE); //  right
-    init_pair(3, COLOR_WHITE, COLOR_BLACK); // мячик
-    init_pair(4, COLOR_BLACK, COLOR_BLACK); // бек
+    init_pair(1, COLOR_YELLOW, COLOR_YELLOW);  // поле верх низ право лево
+    init_pair(2, COLOR_RED, COLOR_RED);        // ракетки left
+    init_pair(22, COLOR_BLUE, COLOR_BLUE);     //  right
+    init_pair(3, COLOR_WHITE, COLOR_BLACK);    // мячик
+    init_pair(4, COLOR_BLACK, COLOR_BLACK);    // бек
 
     init_pair(5, COLOR_GREEN, COLOR_GREEN);
 
-    
     noecho();
     cbreak();
 
     series();
     clear();
 
-    endwin(); 
+    endwin();
     clear();
 }
 
@@ -61,37 +60,36 @@ void draw_pong() {
                 attron(COLOR_PAIR(1));
                 mvaddch(y, x, ' ');
                 attroff(COLOR_PAIR(1));
-            } else if (x == 3 && (y >= l_racket &&  y <= l_racket + 2)) { // ракетки
+            } else if (x == 3 && (y >= l_racket && y <= l_racket + 2)) {  // ракетки
                 attron(COLOR_PAIR(2));
                 mvaddch(y, x, ' ');
                 attroff(COLOR_PAIR(2));
-            } else if (x == WIDTH - 4 && (y >= r_racket &&  y <= r_racket + 2)) {
+            } else if (x == WIDTH - 4 && (y >= r_racket && y <= r_racket + 2)) {
                 attron(COLOR_PAIR(22));
                 mvaddch(y, x, ' ');
                 attroff(COLOR_PAIR(22));
-            } else if (x == x_ball && y == y_ball) { // мяч
+            } else if (x == x_ball && y == y_ball) {  // мяч
                 mvaddch(y, x, 'o');
-            } else if (x == WIDTH/2) {
+            } else if (x == WIDTH / 2) {
                 mvaddch(y, x, '|');
-            } else { 
+            } else {
                 attron(COLOR_PAIR(4));
                 printw(" ");
                 attroff(COLOR_PAIR(4));
             }
-
         }
     }
     printw("\n");
-} 
+}
 
 void series() {
-    while(!(l_res >= 5 || r_res >= 5)) {
+    while (!(l_res >= 5 || r_res >= 5)) {
         draw_pong();
-        instruction(l_res, r_res);        
+        instruction(l_res, r_res);
         ball_move();
 
-        if(!(racket_move())) {
-        break;
+        if (!(racket_move())) {
+            break;
         }
         mvaddstr(87, 6, "==ИНСТРУКЦИЯ==");
     }
@@ -136,17 +134,16 @@ void ball_move() {
     }
 
     if (x_ball <= 1) {
-        x_ball = WIDTH/2;
-        y_ball = HEIGHT/2;
+        x_ball = WIDTH / 2;
+        y_ball = HEIGHT / 2;
         r_res += 1;
     }
-    
-    if (x_ball >= WIDTH -2) {
-        x_ball = WIDTH/2;
-        y_ball = HEIGHT/2;
+
+    if (x_ball >= WIDTH - 2) {
+        x_ball = WIDTH / 2;
+        y_ball = HEIGHT / 2;
         l_res += 1;
     }
-
 }
 
 void instruction(int l, int r) {
@@ -154,25 +151,23 @@ void instruction(int l, int r) {
     int y = 4;
     int u = 7;
     int b = 5;
-    attron(COLOR_PAIR(5)); // верх
+    attron(COLOR_PAIR(5));  // верх
     mvhline(y, x, ' ', 40);
     mvvline(y + 1, x, ' ', 17);
-    
+
     mvhline(HEIGHT - y, x, ' ', 40);
     mvvline(y + 1, x + 40 - 1, ' ', 17);
     attroff(COLOR_PAIR(5));
 
-mvaddstr(6 , 87 + u,  "====================");
-mvaddstr(7 , 87 + u,  "       SCORE:       ");
-mvaddstr(8 , 87 + u,  "====================");
-mvprintw(9 , 87 + u,  "=LEFT PLAYER+=== %d =", l);
-mvprintw(10 , 87 + u, "= %d ==RIGHT PLAYER=", r);
+    mvaddstr(6, 87 + u, "====================");
+    mvaddstr(7, 87 + u, "       SCORE:       ");
+    mvaddstr(8, 87 + u, "====================");
+    mvprintw(9, 87 + u, "=LEFT PLAYER+=== %d =", l);
+    mvprintw(10, 87 + u, "= %d ==RIGHT PLAYER=", r);
 
-mvaddstr(9 + b, 87 + u,  "");
-mvaddstr(10 + b, 87 + u, "CONTROL:");
-mvaddstr(11 + b, 87 + u, "  A / Z   AND   K / M");
-mvaddstr(12 + b, 87 + u, "");
-mvaddstr(13 + b, 87 + u, "Press 'Q' to EXIT");
-
-
+    mvaddstr(9 + b, 87 + u, "");
+    mvaddstr(10 + b, 87 + u, "CONTROL:");
+    mvaddstr(11 + b, 87 + u, "  A / Z   AND   K / M");
+    mvaddstr(12 + b, 87 + u, "");
+    mvaddstr(13 + b, 87 + u, "Press 'Q' to EXIT");
 }
